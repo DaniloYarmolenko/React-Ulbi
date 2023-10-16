b8a7f2ed84ca59e5fb55b455980e5596b8eb6e15
Danilo <131854792+DaniloYarmolenko@users.noreply.github.com>
2023-10-16 10:54:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -16,31 +16,22 @@ function App() {
     {id:3, title:'JavaScript 3', body:'Description'}
   ])
 
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = () => {
+
+  }
+
       return (
         <div className="App">
-            <PostForm/>
+            <PostForm create={createPost}/>
             <PostList posts = {posts} title={'Список постов 1'}/>
-
         </div>
       );
     }
 
-function App() {
-
-    const [posts, setPosts] = useState([
-        {id:1, title:'JavaScript', body:'Description'},
-        {id:2, title:'JavaScript 2', body:'Description'},
-        {id:3, title:'JavaScript 3', body:'Description'}
-    ])
-
-    return (
-        <div className="App">
-            <PostForm/>
-            <PostList posts = {posts} title={'Список постов 1'}/>
-
-        </div>
-    );
-}
 
 
 export default App;
